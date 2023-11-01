8ecbb8174284592b91cd604f37b7786b81f3a9b6
kapich1192 <kapich1192@yandex.ru>
2023-11-01 12:31:48 +0300

    feat: Добавил решение и тесты задачи ЯА 3_1_А Перестановки

---
+++ src/main/java/org/example/yandex/academy/permutations_3_1_a/Task_3_1_A.java
@@ -0,0 +1,27 @@
+package org.example.yandex.academy.permutations_3_1_a;
+
+import java.util.Scanner;
+
+public class Task_3_1_A {
+    static int num;
+    static int res;
+    public static void main(String[] args) {
+        num = input();
+        res = solution(num);
+        output();
+    }
+    public static int input() {
+        Scanner console = new Scanner(System.in);
+        int res = console.nextInt();
+        return res;
+    }
+    public static int solution(int num) {
+        int res = 1;
+        for(int i = 1; i <= num; i++) res *= i;
+      return res;
+    }
+
+    public static void output() {
+        System.out.print(res);
+    }
+}
---
+++ src/test/java/org/example/yandex/academy/permutations_3_1_a/Task_3_1_ATest.java
@@ -0,0 +1,31 @@
+package org.example.yandex.academy.permutations_3_1_a;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Task_3_1_ATest {
+
+    @Test
+    public void test1() {
+        int assertNum = 1;
+        int assertRes = Task_3_1_A.solution(assertNum);
+        int equalsRes = 1;
+        assertEquals(assertRes,equalsRes);
+    }
+    @Test
+    public void test2() {
+        int assertNum = 2;
+        int assertRes = Task_3_1_A.solution(assertNum);
+        int equalsRes = 2;
+        assertEquals(assertRes,equalsRes);
+    }
+    @Test
+    public void test3() {
+        int assertNum = 3;
+        int assertRes = Task_3_1_A.solution(assertNum);
+        int equalsRes = 6;
+        assertEquals(assertRes,equalsRes);
+    }
+
+}
